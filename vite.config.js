fc30c61f9d8e47d2ae0992b3e26a7051b939d9e7
viannatiago456-arch <viannatiago456@gmail.com>
2026-05-04 18:35:06 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ox-dashboard/',
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
-      }
-    }
-  }
+  base: '/ox-dashboard/', // 🔥 MUITO IMPORTANTE
 })
